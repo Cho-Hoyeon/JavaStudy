8dda5315d01eb99790648fc628388c7bba6d66e7
Cho-Hoyeon <adrenainemaker@gmail.com>
2024-08-19 00:35:40 +0900

    20240819_2nd

---
+++ PJTests/src/pj_20240818/bj_10869.java
@@ -0,0 +1,24 @@
+package pj_20240818;
+import java.util.Scanner;
+
+class bj_10869 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = 1;
+		int B = 1;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+		
+		sc.close();
+
+	}
+
+}
---
+++ PJTests/src/pj_20240818/bj_2557.java
@@ -0,0 +1,6 @@
+package pj_20240818;
+class bj_2557 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
